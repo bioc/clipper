6c1770623bb7278d254b811d759b4bc3e8fe6f63
Paolo Martini <paolo.cave@gmail.com>
2017-10-20 18:49:50 +0200

    Changes in clipper to be compliant with graphite.

--- R/plotInCytoscape.R
+++ R/plotInCytoscape.R
@@ -15,29 +15,35 @@
 # You should have received a copy of the GNU Affero General Public
 # License along with clipper. If not, see <http://www.gnu.org/licenses/>.
 
-plotInCytoscape <- function(graph, path, color="#6699FF", main="graph", layout="jgraph-spring"){
-    if(requireNamespace("RCytoscape", quitely=TRUE)) {
+plotInCytoscape <- function(graph, path, color="#6699FF", main="graph", layout="force-directed"){
+    if(requireNamespace("RCy3", quitely=TRUE)) {
            path <- as.character(path)
            genesInvolved <- retrieveInvolvedGenes(path)
            
            g <- markMultiple(graph)
-           g <- RCytoscape::initEdgeAttribute(g, "edgeType", "char", "undefined")
-           g <- RCytoscape::initEdgeAttribute(g, "weight", "numeric", 1)
+           g <- RCy3::initEdgeAttribute(g, "edgeType", "char", "undefined")
+           g <- RCy3::initEdgeAttribute(g, "weight", "numeric", 1)
            
-           cy <- RCytoscape::CytoscapeConnection()
+           cy <- RCy3::CytoscapeConnection()
+
+           possible.layout.names <- RCy3::getLayoutNames(cy)
+           if (!layout %in% possible.layout.names) {
+               warning(paste0("Layout \"",layout,"\" not found. Grid layout setted. Please chose one of the following. \n", paste(possible.layout.names, collapse=", ")))
+               layout="grid"
+           }
            
-           if (main %in% as.character(RCytoscape::getWindowList(cy)))
-               RCytoscape::deleteWindow(cy, main)
+           if (main %in% as.character(RCy3::getWindowList(cy)))
+               RCy3::deleteWindow(cy, main)
            
-           w <- RCytoscape::new.CytoscapeWindow(main, g)
-           RCytoscape::deleteNodeAttribute(w,"node.fillColor")
-           RCytoscape::displayGraph(w)
+           w <- RCy3::CytoscapeWindow(main, g)
+           RCy3::deleteNodeAttribute(w,"node.fillColor")
+           RCy3::displayGraph(w)
   
-           RCytoscape::setNodeColorDirect(w, genesInvolved, color)
-           RCytoscape::layoutNetwork(w, layout.name=layout)
-           RCytoscape::redraw(w)
+           RCy3::setNodeColorDirect(w, genesInvolved, color)
+           RCy3::layoutNetwork(w, layout.name=layout)
+           RCy3::redraw(w)
        } else {
-           stop("Package RCytoscape not installed. Please install.")
+           stop("Package RCy3 not installed. Please install.")
        }
 }
 
